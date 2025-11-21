5388c943ea77bae2a442ed572d56b498b780fc6c
Joemarie <joemarie.r@techguys.work>
2025-11-21 18:37:03 +0800

    change

--- src/data/skillsData.js
+++ src/data/skillsData.js
@@ -3,23 +3,29 @@ import {
   FaJs,
   FaHtml5,
   FaCss3Alt,
-  FaLaravel,
   FaWordpress,
   FaGithub,
   FaFigma,
+  FaReact,
 } from "react-icons/fa";
+import { SiVite, SiTailwindcss, SiVercel } from "react-icons/si";
+
 import { SiKotlin, SiMysql, SiSqlite, SiCanva, SiXampp } from "react-icons/si";
 
 export const skillsGroups = [
   {
     title: "Languages",
     skills: [
-      { name: "PHP", icon: FaPhp, color: "#777BB4" },
+      { name: "PHP", icon: FaPhp, color: "#8892be" },
       { name: "JavaScript", icon: FaJs, color: "#F7DF1E" },
       { name: "Kotlin", icon: SiKotlin, color: "#7F52FF" },
       { name: "HTML", icon: FaHtml5, color: "#E34F26" },
       { name: "CSS", icon: FaCss3Alt, color: "#1572B6" },
     ],
+  },
+  {
+    title: "Library",
+    skills: [{ name: "React", icon: FaReact, color: "#61DBFB" }],
   },
   {
     title: "Frameworks & Tools",
@@ -29,6 +35,8 @@ export const skillsGroups = [
         icon: FaWordpress,
         color: "#21759B",
       },
+      { name: "Vite", icon: SiVite, color: "#41D1FF " },
+      { name: "TailWind CSS", icon: SiTailwindcss, color: "#38bdf8 " },
     ],
   },
   {
@@ -45,7 +53,7 @@ export const skillsGroups = [
   {
     title: "UI/UX & Design",
     skills: [
-      { name: "Canva", icon: SiCanva, color: "#00C4CC" },
+      { name: "Canva", icon: SiCanva, color: "#07B9CE" },
       { name: "Figma", icon: FaFigma, color: "#F24E1E" },
     ],
   },
@@ -55,6 +63,7 @@ export const skillsGroups = [
       { name: "XAMPP", icon: SiXampp, color: "#FB7A24" },
       { name: "GitHub Pages", icon: FaGithub, color: "#181717" },
       { name: "WordPress Hosting", icon: FaWordpress, color: "#21759B" },
+      { name: "Vercel app", icon: SiVercel, color: "#000000" },
     ],
   },
   {
